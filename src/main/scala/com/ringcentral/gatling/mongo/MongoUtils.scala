9b6106a80ed1a6bbcff521e55852dfdfee9ac24e
mskonovalov <mskonovalov@gmail.com>
2017-03-30 14:01:06 +0300

    small refactoring

--- src/main/scala/com/ringcentral/gatling/mongo/MongoUtils.scala
+++ src/main/scala/com/ringcentral/gatling/mongo/MongoUtils.scala
@@ -3,8 +3,8 @@ package com.ringcentral.gatling.mongo
 import reactivemongo.api.MongoConnection.{ParsedURI, URIParsingException}
 import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
 // fixme remove global context
@@ -12,7 +12,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object MongoUtils {
 
-  private val DEFAULT_PORT: Int = 27017
+  private val defaultPort: Int = 27017
   private lazy val mongoDriver = new MongoDriver()
 
   private def establishConnection(uri: ParsedURI, dbName: String, connectionTimeout: FiniteDuration): DefaultDB = {
@@ -27,7 +27,7 @@ object MongoUtils {
         throw new IllegalStateException(s"Can't connect to database ${printHosts(uri.hosts)}: ${err.getMessage}", err)
     }
 
-  private def printHosts(hosts: List[(String, Int)]): String = hosts.map(item => s"${item._1}:${item._2}").mkString(", ")
+  private def printHosts(hosts: List[(String, Int)]): String = hosts.map { case (host, port) => s"$host:$port" }.mkString(", ")
 
   def connectToDB(uri: ParsedURI, connectionTimeout: FiniteDuration): DefaultDB =
     uri.db match {
@@ -43,7 +43,7 @@ object MongoUtils {
         host -> Try(port.toInt).filter(p => p > 0 && p < 65536)
           .getOrElse(throw new URIParsingException(s"Could not parse hosts '$hosts' from URI: invalid port '$port'"))
       case host :: Nil =>
-        host -> DEFAULT_PORT
+        host -> defaultPort
       case _ => throw new URIParsingException(s"Could not parse hosts from URI: invalid definition '$hosts'")
     }
   }
--- src/main/scala/com/ringcentral/gatling/mongo/check/count/MongoCountCheckBuilder.scala
+++ src/main/scala/com/ringcentral/gatling/mongo/check/count/MongoCountCheckBuilder.scala
@@ -3,20 +3,20 @@ package com.ringcentral.gatling.mongo.check.count
 import com.ringcentral.gatling.mongo.check.{MongoCheck, MongoCheckBuilders}
 import com.ringcentral.gatling.mongo.response.{MongoCountResponse, MongoResponse}
 import io.gatling.commons.validation._
+import io.gatling.core.Predef._
 import io.gatling.core.check.DefaultFindCheckBuilder
 import io.gatling.core.check.extractor.{Extractor, SingleArity}
-import io.gatling.core.session._
 
 object MongoCountCheckBuilder {
 
-  val countExtractor: Expression[Extractor[MongoResponse, Int] with SingleArity with Object {def name: String; def apply(prepared: MongoResponse): Validation[Some[Int]]}] = new Extractor[MongoResponse, Int] with SingleArity {
+  private val countExtractor: Extractor[MongoResponse, Int] = new Extractor[MongoResponse, Int] with SingleArity {
     override def name: String = "count"
 
     override def apply(prepared: MongoResponse): Validation[Some[Int]] = prepared match {
       case response: MongoCountResponse => Some(response.value).success
       case _ => "Response wasn't received".failure
     }
-  }.expressionSuccess
+  }
 
   val count = new DefaultFindCheckBuilder[MongoCheck, MongoResponse, MongoResponse, Int](MongoCheckBuilders.countExtender, MongoCheckBuilders.passThroughResponsePreparer, countExtractor)
 }
